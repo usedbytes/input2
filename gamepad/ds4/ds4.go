95429513fa84d34ceb61a95c3aaa6326bb3f26ba
Brian Starkey <stark3y@gmail.com>
2017-07-30 23:21:50 +0100

    ds4: Split out LED code

--- gamepad/ds4/ds4.go
+++ gamepad/ds4/ds4.go
@@ -2,13 +2,11 @@ package ds4
 
 import (
 	"fmt"
-	"image/color"
 	"log"
 	"os"
 	"path/filepath"
 	"regexp"
 	"sync"
-	"strings"
 	"time"
 	"github.com/gvalkov/golang-evdev"
 	"github.com/jochenvg/go-udev"
@@ -277,46 +275,6 @@ func (g *Gamepad) initEvdev() error {
 	return fmt.Errorf("Couldn't find event device")
 }
 
-func (g *Gamepad) initLeds() error {
-	var red, green, blue, global string
-	leds, err := filepath.Glob(g.sysdir + "/leds/*")
-	if err != nil {
-		return err
-	}
-	if len(leds) != 4 {
-		return fmt.Errorf("Expected 4 LEDs")
-	}
-
-	for _, l := range leds {
-		if strings.HasSuffix(l, ":red") {
-			red = l
-			continue
-		}
-		if strings.HasSuffix(l, ":green") {
-			green = l
-			continue
-		}
-		if strings.HasSuffix(l, ":blue") {
-			blue = l
-			continue
-		}
-		if strings.HasSuffix(l, ":global") {
-			global = l
-			continue
-		}
-	}
-	if red == "" || green == "" || blue == "" || global == "" {
-		return fmt.Errorf("Couldn't match LED names")
-	}
-
-	g.led, err = led.NewRGBLED(red, green, blue, global)
-	if (err != nil) {
-		return fmt.Errorf("Couldn't get leds: %s\n", err.Error())
-	}
-
-	return nil
-}
-
 func (g *Gamepad) initBattery() error {
 	psus, err := filepath.Glob(g.sysdir + "/power_supply/*")
 	if err != nil {
@@ -409,30 +367,6 @@ func (g *Gamepad) CreateRumbleEffect(strongMag, weakMag float32, duration time.D
 	}
 
 	return effect, nil
-}
-
-func (g *Gamepad) GetLED() led.LinuxLED {
-	return g.led
-}
-
-func (g *Gamepad) SetColor(color color.Color) error {
-	return g.led.SetColor(color)
-}
-
-func (g *Gamepad) SetBrightness(brightness float32) error {
-	return g.led.SetBrightness(brightness)
-}
-
-func (g *Gamepad) Off() error {
-	return g.led.Off()
-}
-
-func (g *Gamepad) SetTrigger(trigger led.Trigger) error {
-	return g.led.SetTrigger(trigger)
-}
-
-func (g *Gamepad) GetTrigger() led.Trigger {
-	return g.led.GetTrigger()
 }
 
 func (g *Gamepad) GetBattery() battery.Battery {
---
+++ gamepad/ds4/led.go
@@ -0,0 +1,73 @@
+package ds4
+
+import (
+	"fmt"
+	"image/color"
+	"path/filepath"
+	"strings"
+	"github.com/usedbytes/linux-led"
+)
+
+func (g *Gamepad) initLeds() error {
+	var red, green, blue, global string
+	leds, err := filepath.Glob(g.sysdir + "/leds/*")
+	if err != nil {
+		return err
+	}
+	if len(leds) != 4 {
+		return fmt.Errorf("Expected 4 LEDs")
+	}
+
+	for _, l := range leds {
+		if strings.HasSuffix(l, ":red") {
+			red = l
+			continue
+		}
+		if strings.HasSuffix(l, ":green") {
+			green = l
+			continue
+		}
+		if strings.HasSuffix(l, ":blue") {
+			blue = l
+			continue
+		}
+		if strings.HasSuffix(l, ":global") {
+			global = l
+			continue
+		}
+	}
+	if red == "" || green == "" || blue == "" || global == "" {
+		return fmt.Errorf("Couldn't match LED names")
+	}
+
+	g.led, err = led.NewRGBLED(red, green, blue, global)
+	if (err != nil) {
+		return fmt.Errorf("Couldn't get leds: %s\n", err.Error())
+	}
+
+	return nil
+}
+
+func (g *Gamepad) GetLED() led.LinuxLED {
+	return g.led
+}
+
+func (g *Gamepad) SetColor(color color.Color) error {
+	return g.led.SetColor(color)
+}
+
+func (g *Gamepad) SetBrightness(brightness float32) error {
+	return g.led.SetBrightness(brightness)
+}
+
+func (g *Gamepad) Off() error {
+	return g.led.Off()
+}
+
+func (g *Gamepad) SetTrigger(trigger led.Trigger) error {
+	return g.led.SetTrigger(trigger)
+}
+
+func (g *Gamepad) GetTrigger() led.Trigger {
+	return g.led.GetTrigger()
+}
